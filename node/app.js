5945a926de14e8fc7164e995e54e588c797918e1
Ramya Singavarapu <ramyalakshmisingavarapu@gmail.com>
2024-01-04 16:55:19 +0100

    Demonstrates sharing JavaScript code between different files

---
+++ node/app.js
@@ -0,0 +1,2 @@
+const math = require('./math')
+console.log(math);
---
+++ node/math.js
@@ -0,0 +1,4 @@
+const add = (x, y) => console.log(x + y);
+const { PI } = 3.141523;
+const square = (y) => console.log(y * y);
+module.exports.add = add;
