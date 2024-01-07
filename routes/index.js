711aebc3f5e1f2d310ca92335eb56655ba160946
Ramya Singavarapu <ramyalakshmisingavarapu@gmail.com>
2024-01-07 11:22:56 +0100

    Implement basic routing in Express app with GET and POST endpoints

---
+++ routes/index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+app.get('/cats', (req, res) => {
+    res.send("MEOW");
+})
+app.get('/dogs', (req, res) => {
+    res.send("BOWydrfhtjg!!");
+})
+app.post('/cats', (req, res) => {
+    res.send("POST MEOW!!");
+})
+app.get('/r/:subreddit', (req, res) => {
+    const { subreddit } = req.params;
+    res.send(`Hello from the ${subreddit} page`);
+})
+app.get('/r/:subreddit/:postId', (req, res) => {
+    const { subreddit, postId } = req.params;
+    res.send(`Hello from the ${subreddit} page with ${postId} id`);
+})
+
+app.get('*', (req, res) => {
+    res.send("SORRY!! WE CANNOT FIND THE PATH");
+})
+app.listen(3030, () => {
+    console.log("LISTENING ON PORT 3030");
+})
