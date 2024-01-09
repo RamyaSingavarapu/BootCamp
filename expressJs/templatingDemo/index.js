a89515c55d107ce17f292ae6509d69b9bd88f2a0
Ramya Singavarapu <ramyalakshmisingavarapu@gmail.com>
2024-01-09 10:50:45 +0100

    Add static file serving to Express app with EJS views

--- expressJs/templatingDemo/index.js
+++ expressJs/templatingDemo/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const redditData = require('./data.json');
+app.use(express.static(path.join(__dirname, 'public')))
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, '/views'));
 app.get('/', (req, res) => {
