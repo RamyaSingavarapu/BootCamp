f10ea29e7745d05f0303ec0f99036a9e6c0f4383
Ramya Singavarapu <ramyalakshmisingavarapu@gmail.com>
2024-01-11 17:51:42 +0100

    Implemented route for displaying individual comments by ID

--- expressJs/restDemo/index.js
+++ expressJs/restDemo/index.js
@@ -3,18 +3,22 @@ const app = express();
 const path = require('path');
 const comments = [
     {
+        id: 1,
         username: "Todd",
         comment: "lol that is so funny!"
     },
     {
+        id: 2,
         username: "Skyler",
         comment: "Id like to go bird watching with my dog"
     },
     {
+        id: 3,
         username: "sk79gsl",
         comment: "Please delete your account! Todd"
     },
     {
+        id: 4,
         username: "onlysayswoof",
         comment: "woof woof woof"
     }
@@ -34,6 +38,11 @@ app.post('/comments', (req, res) => {
     const { username, comment } = req.body
     comments.push({ username, comment })
     res.redirect('/comments')
+})
+app.get('/comments/:id', (req, res) => {
+    const { id } = req.params;
+    const comment = comments.find(c => c.id === parseInt(id));
+    res.render('comments/show', { comment })
 })
 app.get('/tacos', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/restDemo.html'))
