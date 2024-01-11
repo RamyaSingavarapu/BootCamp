a4af32734dac362d49a608c6a5ae10f796e208d6
Ramya Singavarapu <ramyalakshmisingavarapu@gmail.com>
2024-01-11 18:27:35 +0100

    added description

--- expressJs/restDemo/index.js
+++ expressJs/restDemo/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const { v4: uuid } = require('uuid');
+const { v4: uuid } = require('uuid');//set uuid = (require('uuid')).v4 (i.e)v4 version of uuid
 const comments = [
     {
         id: uuid(),
@@ -30,20 +30,20 @@ app.use(express.urlencoded({ extended: true }))
 app.use(express.json());
 app.set('views', path.join(__dirname, '/views'));
 app.get('/comments', (req, res) => {
-    res.render('comments/index', { comments })
+    res.render('comments/index', { comments }) //passing comments array as an object
 })
 app.get('/comments/new', (req, res) => {
     res.render('comments/new')
 })
 app.post('/comments', (req, res) => {
     const { username, comment } = req.body
-    comments.push({ username, comment, id: uuid() })
+    comments.push({ username, comment, id: uuid() })//pushing object to an comments array
     res.redirect('/comments')
 })
 app.get('/comments/:id', (req, res) => {
     const { id } = req.params;
-    const comment = comments.find(c => c.id === id);
-    res.render('comments/show', { comment })
+    const comment = comments.find(c => c.id === id);//finds matching array element(i.e object)
+    res.render('comments/show', { comment }) // {comment}= passing an comment object
 })
 app.get('/tacos', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/restDemo.html'))
