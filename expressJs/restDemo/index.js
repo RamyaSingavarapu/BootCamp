47f0ffd606ffdccf877bbe6b108e57563e723680
Ramya Singavarapu <ramyalakshmisingavarapu@gmail.com>
2024-01-11 18:13:21 +0100

    Integrated UUID for Unique Comment Identification

--- expressJs/restDemo/index.js
+++ expressJs/restDemo/index.js
@@ -1,24 +1,25 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const { v4: uuid } = require('uuid');
 const comments = [
     {
-        id: 1,
+        id: uuid(),
         username: "Todd",
         comment: "lol that is so funny!"
     },
     {
-        id: 2,
+        id: uuid(),
         username: "Skyler",
         comment: "Id like to go bird watching with my dog"
     },
     {
-        id: 3,
+        id: uuid(),
         username: "sk79gsl",
         comment: "Please delete your account! Todd"
     },
     {
-        id: 4,
+        id: uuid(),
         username: "onlysayswoof",
         comment: "woof woof woof"
     }
@@ -36,12 +37,12 @@ app.get('/comments/new', (req, res) => {
 })
 app.post('/comments', (req, res) => {
     const { username, comment } = req.body
-    comments.push({ username, comment })
+    comments.push({ username, comment, id: uuid() })
     res.redirect('/comments')
 })
 app.get('/comments/:id', (req, res) => {
     const { id } = req.params;
-    const comment = comments.find(c => c.id === parseInt(id));
+    const comment = comments.find(c => c.id === id);
     res.render('comments/show', { comment })
 })
 app.get('/tacos', (req, res) => {
