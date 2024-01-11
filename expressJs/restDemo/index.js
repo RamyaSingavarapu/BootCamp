599b3da239ab2805772dadcbefaaed2e35f2fe5c
Ramya Singavarapu <ramyalakshmisingavarapu@gmail.com>
2024-01-11 11:12:23 +0100

    Implemented comments display feature using EJS templates

--- expressJs/restDemo/index.js
+++ expressJs/restDemo/index.js
@@ -1,10 +1,34 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const comments = [
+    {
+        name: "Todd",
+        comment: "lol that is so funny!"
+    },
+    {
+        name: "Skyler",
+        comment: "Id like to go bird watching with my dog"
+    },
+    {
+        name: "sk79gsl",
+        comment: "Please delete your account! Todd"
+    },
+    {
+        name: "onlysayswoof",
+        comment: "woof woof woof"
+    }
+]
+
+app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, '/public')));
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json());
 app.set('views', path.join(__dirname, '/views'));
+app.get('/comments', (req, res) => {
+    res.render('comments/index', { comments })
+
+})
 app.get('/tacos', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/restDemo.html'))
 })
