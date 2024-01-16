29c596d2fb766126084474920e4c08f24e14adbf
Ramya Singavarapu <ramyalakshmisingavarapu@gmail.com>
2024-01-16 12:14:29 +0100

    Implement Movie schema and add sample Amadeus movie entry

--- mangooseBascis/index.js
+++ mangooseBascis/index.js
@@ -1,7 +1,15 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://127.0.0.1:27j017/movieApp').then(() => {
+mongoose.connect('mongodb://127.0.0.1:27017/movieApp').then(() => {
     console.log("Connection Open!!")
 }).catch(() => {
     console.log("OH NO ERROR!!");
     console.log(err);
 });
+const movieSchema = new mongoose.Schema({
+    title: 'String',
+    year: Number,
+    score: Number,
+    rating: 'String'
+})
+const movie = mongoose.model('Movie', movieSchema);
+const amadeus = new movie({ title: 'Amadeus', year: 1986, score: 9.2, rating: 'R' });
