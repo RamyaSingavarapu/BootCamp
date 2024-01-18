9ca82a5688f487cf182396677622157871345281
Ramya Singavarapu <ramyalakshmisingavarapu@gmail.com>
2024-01-18 17:41:45 +0100

    Expand Product schema with new fields and implement save functionality

--- mangooseBascis/product.js
+++ mangooseBascis/product.js
@@ -15,10 +15,25 @@ var productSchema = new mongoose.Schema({
     price: {
         type: Number,
         required: true
+    },
+    categories: [String],
+    onSale: {
+        type: Boolean,
+        default: false
+    },
+    qty: {
+        online: {
+            type: Number,
+            default: 0
+        },
+        instore: {
+            type: Number,
+            default: 0
+        }
     }
 })
 var Product = mongoose.model('Product', productSchema);
-var bike = new Product({ name: 'RoyalEnfield', price: 500 })
+var bike = new Product({ name: 'RoyalEnfield', price: 500, categories: ['cycling', 'riding'], onSale: 0, qty: { online: 0, inStore: 0 } })
 bike.save().then((data) => {
     console.log("IT WORKED")
     console.log(data)
