247f09dc9f68b1964bdb72f299fc7014646f4369
Ramya Singavarapu <ramyalakshmisingavarapu@gmail.com>
2024-01-22 10:05:45 +0100

    Add Person model with fullName virtual property and create Tammy instance

---
+++ mangooseBascis/person.js
@@ -0,0 +1,20 @@
+var mongoose = require('mongoose');
+
+mongoose.connect('mongodb://127.0.0.1:27017/movieApp').then(() => {
+    console.log("Connection Open!!")
+}).catch((m) => {
+    console.log("OH NO ERROR!!");
+    console.log(m);
+});
+
+var personSchema = new mongoose.Schema({
+    first: String,
+    last: String
+})
+
+personSchema.virtual('fullName').get(function () {
+    return `${this.first} ${this.last}`;
+})
+
+var Person = mongoose.model('Person', personSchema);
+
