6f5d36aedb2ce52bddeec77597cd62392cf45429
Ramya Singavarapu <ramyalakshmisingavarapu@gmail.com>
2024-01-22 22:48:15 +0100

    Implement product creation routes with form display and data handling

--- mangooseBascis/Mongoose_Express/index.js
+++ mangooseBascis/Mongoose_Express/index.js
@@ -14,10 +14,22 @@ mongoose.connect('mongodb://127.0.0.1:27017/farmStand').then(() => {
 
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
+app.use(express.urlencoded({ extended: true }))
 
 app.get('/products', async (req, res) => {
     const products = await Product.find({});
     res.render('products/index', { products })
+})
+
+app.get('/products/new', (req, res) => {
+    res.render('products/new');
+})
+
+app.post('/products', async (req, res) => {
+    const newProduct = new Product(req.body);
+    await newProduct.save();
+    res.redirect(`/products/${newProduct._id}`);
+
 })
 
 app.get('/products/:id', async (req, res) => {
