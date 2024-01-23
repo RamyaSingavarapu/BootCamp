544e235d333dc3dda8e5aff195f63656bc876b20
Ramya Singavarapu <ramyalakshmisingavarapu@gmail.com>
2024-01-23 01:17:30 +0100

    Implement category-based filtering for product listing in Express app

--- mangooseBascis/Mongoose_Express/index.js
+++ mangooseBascis/Mongoose_Express/index.js
@@ -22,8 +22,14 @@ app.use(methodOverride('_method'));
 
 
 app.get('/products', async (req, res) => {
-    const products = await Product.find({});
-    res.render('products/index', { products })
+    const { category } = req.query;
+    if (category) {
+        const products = await Product.find({ category });
+        res.render('products/index', { products, category })
+    } else {
+        const products = await Product.find({});
+        res.render('products/index', { products, category })
+    }
 })
 
 app.get('/products/new', (req, res) => {
