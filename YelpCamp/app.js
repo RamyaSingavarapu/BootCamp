415fd5c5b6f891168ec20678cd221aeb7c406a5c
Ramya Singavarapu <ramyalakshmisingavarapu@gmail.com>
2024-01-23 01:53:58 +0100

    Initial commit: Set up Express server and configure EJS view engine

---
+++ YelpCamp/app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+
+app.get('/', (req, res) => {
+    res.render('home');
+})
+
+app.listen(2000, () => {
+    console.log("Serving on port 2000");
+})
