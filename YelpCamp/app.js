c3fdccf8c5031ae40404a01dbbf2e658af1633fc
Ramya Singavarapu <ramyalakshmisingavarapu@gmail.com>
2024-01-24 16:27:56 +0100

    Implement new campground creation with form submission and redirection

--- YelpCamp/app.js
+++ YelpCamp/app.js
@@ -15,6 +15,7 @@ db.once("open", () => {
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
+app.use(express.urlencoded({ extended: true }));
 
 
 app.get('/', (req, res) => {
@@ -26,10 +27,19 @@ app.get('/campgrounds', async (req, res) => {
     res.render('campgrounds/index', { campground });
 })
 
+app.get('/campgrounds/new', (req, res) => {
+    res.render('campgrounds/new');
+})
+
+app.post('/campgrounds', async (req, res) => {
+    const campground = new Campground(req.body.campground);
+    await campground.save();
+    res.redirect(`/campgrounds/${campground._id}`);
+})
+
 app.get('/campgrounds/:id', async (req, res) => {
     const { id } = req.params;
     const campground = await Campground.findById({ _id: id });
-    console.log(campground);
     res.render('campgrounds/show', { campground });
 })
 
