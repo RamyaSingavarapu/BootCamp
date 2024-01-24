81e5113de8bbe4464c9c80313cbb12ed2b58470c
Ramya Singavarapu <ramyalakshmisingavarapu@gmail.com>
2024-01-24 17:28:25 +0100

    Implement campground deletion functionality with redirect to index

--- YelpCamp/app.js
+++ YelpCamp/app.js
@@ -57,6 +57,12 @@ app.put('/campgrounds/:id', async (req, res) => {
     res.redirect(`/campgrounds/${campground._id}`)
 })
 
+app.delete('/campgrounds/:id', async (req, res) => {
+    const { id } = req.params;
+    await Campground.findByIdAndDelete({ _id: id });
+    res.redirect('/campgrounds');
+})
+
 app.listen(2000, () => {
     console.log("Serving on port 2000");
 })
