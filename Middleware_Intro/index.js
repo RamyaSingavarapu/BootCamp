f2cda27423cf6a174c480b3521bedd903491c4e0
Ramya Singavarapu <ramyalakshmisingavarapu@gmail.com>
2024-01-24 23:05:21 +0100

    Add custom logging middleware and request time tracking to Express server

--- Middleware_Intro/index.js
+++ Middleware_Intro/index.js
@@ -4,11 +4,34 @@ const morgan = require('morgan');
 
 app.use(morgan('dev'));
 
+app.use((req, res, next) => {
+    req.requestTime = Date.now();
+    console.log(req.method, req.path);
+    next();
+})
+
+// app.use((req, res, next) => {
+//     console.log("THIS IS MY FIRST MIDDLE WARE");
+//     next();
+// })
+// app.use((req, res, next) => {
+//     console.log("THIS IS MY SECOND MIDDLE WARE");
+//     return next();
+//     console.log("THIS IS MY SECOND MIDDLE WARE - AFTER CALLING");
+// })
+// app.use((req, res, next) => {
+//     console.log("THIS IS MY THIRD MIDDLE WARE");
+//     next();
+// })
+
+
 app.get('/', (req, res) => {
+    console.log(req.requestTime);
     res.send("HOME PAGE!!")
 })
 
 app.get('/chicken', (req, res) => {
+    console.log(req.requestTime);
     res.send('chick chick');
 })
 
