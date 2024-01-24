afdc1c2289917e0abcdc2e6b89a34bf9f3a58d47
Ramya Singavarapu <ramyalakshmisingavarapu@gmail.com>
2024-01-24 12:06:27 +0100

    Implement database seeding for campgrounds with sample data

--- YelpCamp/app.js
+++ YelpCamp/app.js
@@ -4,15 +4,12 @@ const path = require('path');
 const mongoose = require('mongoose');
 const Campground = require('./models/campground');
 
-mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp', {
-    //useNewUrlParser: true,
-    // useCreateIndex: true,
-    // useUnifiedTopology: true
-}).then(() => {
-    console.log("Database Connected!!")
-}).catch((m) => {
-    console.log("MONGO CONNECTION ERROR!!");
-    console.log(m);
+mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp');
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+    console.log("Database connected");
 });
 
 
