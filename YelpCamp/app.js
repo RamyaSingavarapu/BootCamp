457f124f23bf2a787938c97c83d9cb023b476268
Ramya Singavarapu <ramyalakshmisingavarapu@gmail.com>
2024-01-25 11:04:34 +0100

    Refactor views with ejs-mate

--- YelpCamp/app.js
+++ YelpCamp/app.js
@@ -4,6 +4,8 @@ const path = require('path');
 const mongoose = require('mongoose');
 const Campground = require('./models/campground');
 const methodOverride = require('method-override');
+const ejsMate = require('ejs-mate');
+
 
 mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp');
 
@@ -13,7 +15,7 @@ db.once("open", () => {
     console.log("Database connected");
 });
 
-
+app.engine('ejs', ejsMate);
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.urlencoded({ extended: true }));
