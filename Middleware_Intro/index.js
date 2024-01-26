5a81107c2288675a8b6acff0f801dfb3e7db7048
Ramya Singavarapu <ramyalakshmisingavarapu@gmail.com>
2024-01-26 11:08:24 +0100

    Implement error handling in password verification middleware for Express server

--- Middleware_Intro/index.js
+++ Middleware_Intro/index.js
@@ -53,6 +53,15 @@ app.get('/secret', verifyPassword, (req, res) => {
     res.send('MY SECRET IS: Sometimes I wear a headphones in public so I dont have talk to anyone');
 })
 
+app.use((err, req, res, next) => {
+    console.log('********************************');
+    console.log('**********ERROR*********');
+    console.log('********************************');
+    console.log(err);
+    res.status(500).send('OH NO!! ERROR');
+    next(err);
+})
+
 app.use((req, res) => {
     res.status(404).send('NOT FOUND');
 })
