535880877dffa6f3429163a7de0d6b87ac10869b
Ramya Singavarapu <ramyalakshmisingavarapu@gmail.com>
2024-03-20 16:42:42 +0100

    Cookie Parser Middleware

--- CookiesDemo/index.js
+++ CookiesDemo/index.js
@@ -1,12 +1,17 @@
 const express = require('express');
+const cookieParser = require('cookie-parser');
 const app = express();
 
+app.use(cookieParser());
+
 app.get('/greet', (req, res) => {
-    res.send("HEY THERE!!");
+    const { name = 'NO-NAME' } = req.cookies
+    res.send(`Hi ${name}`);
 })
 
 app.get('/setname', (req, res) => {
     res.cookie('name', 'ramya');
+    res.cookie('animal', 'Tiger');
     res.send("OK!! I SEND YOU A COOKIE");
 })
 
