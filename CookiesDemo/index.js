a9fb39a95acab0ff9107289c27634165e47ccd43
Ramya Singavarapu <ramyalakshmisingavarapu@gmail.com>
2024-03-20 17:08:04 +0100

    Signing Cookies

--- CookiesDemo/index.js
+++ CookiesDemo/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cookieParser = require('cookie-parser');
 const app = express();
 
-app.use(cookieParser());
+app.use(cookieParser('this-is-my-secret'));
 
 app.get('/greet', (req, res) => {
     const { name = 'NO-NAME' } = req.cookies
@@ -13,6 +13,18 @@ app.get('/setname', (req, res) => {
     res.cookie('name', 'ramya');
     res.cookie('animal', 'Tiger');
     res.send("OK!! I SEND YOU A COOKIE");
+})
+
+app.get('/getsignedcookie', (req, res) => {
+    res.cookie('fruit', 'grape', { signed: true });
+    res.send('OK SIGNED YOUR FRUIT COOKIE');
+})
+
+app.get('/verifyfruit', (req, res) => {
+    console.log(req.cookies)
+    console.log(req.signedCookies)
+    res.send(req.signedCookies)
+    //res.send(req.cookies)
 })
 
 app.listen('3900', () => {
