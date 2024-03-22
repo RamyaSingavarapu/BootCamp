8cb87778bb5de3b635f05f8d66f7112880774598
Ramya Singavarapu <ramyalakshmisingavarapu@gmail.com>
2024-03-22 11:19:42 +0100

    More Express Session

--- SessionDemo/index.js
+++ SessionDemo/index.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const app = express();
 const session = require('express-session');
+const sessionOptions = ({ secret: 'thisisnotagoodsecret', resave: false, saveUninitialized: false })
 
-app.use(session({ secret: 'thisisnotagoodsecret' }));
+app.use(session(sessionOptions));
 
 app.get('/viewCount', (req, res) => {
     if (req.session.count) {
@@ -14,6 +15,16 @@ app.get('/viewCount', (req, res) => {
     res.send(`You have viewed this page ${req.session.count} times`);
 })
 
+app.get('/register', (req, res) => {
+    const { username = 'Anonymous' } = req.query;
+    req.session.username = username;
+    res.redirect('/greet');
+})
+
+app.get('/greet', (req, res) => {
+    const { username } = req.session;
+    res.send(`Welcome back, ${username}`);
+})
 app.listen(3000, () => {
     console.log("Listening on port 3000");
 })
