77fe6b99c88ea7c39626f11745c88cdad8144cab
Ramya Singavarapu <ramyalakshmisingavarapu@gmail.com>
2024-03-26 08:18:17 +0100

    LogOut & Require LogIn middleware

--- AuthDemo/index.js
+++ AuthDemo/index.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 const User = require('./models/user');
 const path = require('path');
 const bcrypt = require('bcrypt');
+const session = require('express-session');
 
 mongoose.connect('mongodb://127.0.0.1:27017/authDemo');
 
@@ -16,6 +17,13 @@ db.once("open", () => {
 app.set('view engine', 'ejs')
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.urlencoded({ extended: true }))
+app.use(session({ secret: 'notagoodsecret' }));
+
+const requireLogin = async (req, res, next) => {
+    if (!req.session.user_id) {
+        return res.redirect('/login');
+    } next();
+}
 
 app.get('/', (req, res) => {
     res.send('THIS IS THE HOME PAGE');
@@ -37,28 +45,31 @@ app.post('/register', async (req, res) => {
         password: hash
     })
     await user.save();
+    req.session.user_id = user._id;
     res.redirect('/');
 })
 
 app.post('/login', async (req, res) => {
     const { username, password } = req.body;
-    const user = await User.findOne({ username });
-    const validPassword = await bcrypt.compare(password, user.password);
-    if (validPassword) {
-        res.send('YAY!! WELCOME');
+    const foundUser = await User.findAndValidate(username, password);
+    if (foundUser) {
+        req.session.user_id = foundUser._id;
+        res.redirect('/secret');
     }
     else {
         res.send('TRY AGAIN');
     }
 })
 
-
-
-app.get('/secret', (req, res) => {
-    res.send('THIS IS A SECRET!! CANNOT SEE ME UNLESS YOU ARE LOGGED IN');
+app.get('/secret', requireLogin, (req, res) => {
+    res.render('secret');
 })
 
-
+app.post('/logout', (req, res) => {
+    // req.session.user_id = null;
+    req.session.destroy();
+    res.redirect('/login');
+})
 
 app.listen(4000, () => {
     console.log("SERVING ON PORT 4000")
