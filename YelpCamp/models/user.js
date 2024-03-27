56723af7073a10699f9218676cd9d0ed94b2c5cc
Ramya Singavarapu <ramyalakshmisingavarapu@gmail.com>
2024-03-27 10:37:58 +0100

    Creating User Model

---
+++ YelpCamp/models/user.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const passportLocalMongoose = require('passport-local-mongoose');
+
+const UserSchema = new Schema({
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    }
+})
+
+UserSchema.plugin(passportLocalMongoose);
+
+model.exports = mongoose.Model('User', UserSchema);
