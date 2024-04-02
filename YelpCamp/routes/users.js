0e43f5febcb4b0beadc367985e75aace3a0df2eb
Ramya Singavarapu <ramyalakshmisingavarapu@gmail.com>
2024-04-02 15:08:52 +0200

    Login Routes

--- YelpCamp/routes/users.js
+++ YelpCamp/routes/users.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const passport = require('passport');
 const router = express.Router();
 const catchAsync = require('../utils/catchAsync');
 const User = require('../models/user');
@@ -22,5 +23,14 @@ router.post('/register', catchAsync(async (req, res) => {
     }
 
 }))
+
+router.get('/login', (req, res) => {
+    res.render('users/login');
+})
+
+router.post('/login', passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), (req, res) => {
+    req.flash('success', 'welcome back!');
+    res.redirect('/campgrounds');
+})
 
 module.exports = router;
