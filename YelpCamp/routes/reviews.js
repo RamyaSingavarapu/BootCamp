133c11d2c62a97fc63419d6c8cb03353612fba90
Ramya Singavarapu <ramyalakshmisingavarapu@gmail.com>
2024-04-08 16:39:41 +0200

    Added Reviews Permission

--- YelpCamp/routes/reviews.js
+++ YelpCamp/routes/reviews.js
@@ -7,13 +7,14 @@ const Review = require('../models/reviews');
 const catchAsync = require('../utils/catchAsync');
 const ExpressError = require('../utils/expressError');
 
-const { validateReview } = require('../middleware');
+const { validateReview, isLoggedIn } = require('../middleware');
 
 
 
-router.post('/', validateReview, catchAsync(async (req, res) => {
+router.post('/', isLoggedIn, validateReview, catchAsync(async (req, res) => {
     const campground = await Campground.findById(req.params.id);
     const review = new Review(req.body.review);
+    review.author = req.user._id
     campground.reviews.push(review);
     await review.save();
     await campground.save();
