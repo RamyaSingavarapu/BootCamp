b89af107bc95cf9c73c5258e7b1a08c560877ccb
Ramya Singavarapu <ramyalakshmisingavarapu@gmail.com>
2024-05-16 11:42:16 +0200

    Deleting images backend

--- YelpCamp/controllers/campgrounds.js
+++ YelpCamp/controllers/campgrounds.js
@@ -1,4 +1,5 @@
 const Campground = require('../models/campground');
+const { cloudinary } = require('../cloudinary/');
 
 module.exports.index = async (req, res) => {
     const campground = await Campground.find({});
@@ -50,6 +51,12 @@ module.exports.updateCampground = async (req, res) => {
     const imgs = req.files.map(f => ({ url: f.path, filename: f.filename }));
     campground.images.push(...imgs);
     await campground.save()
+    if (req.body.deleteImages) {
+        for (let filename of req.body.deleteImages) {
+            await cloudinary.uploader.destroy(filename);
+        }
+        await campground.updateOne({ $pull: { images: { filename: { $in: req.body.deleteImages } } } })
+    }
     req.flash('success', 'Successfully updated a campground');
     res.redirect(`/campgrounds/${campground._id}`)
 }
