261fbcb34ee1e97b850f715f3e690a6e05e37777
Ramya Singavarapu <ramyalakshmisingavarapu@gmail.com>
2024-05-16 13:42:02 +0200

    Adding a thumbnail virtual property

--- YelpCamp/models/campground.js
+++ YelpCamp/models/campground.js
@@ -2,14 +2,18 @@ const mongoose = require('mongoose');
 const Review = require('./reviews');
 const Schema = mongoose.Schema;
 
+const ImageSchema = new Schema({
+    url: String,
+    filename: String
+})
+
+ImageSchema.virtual('thumbnail').get(function () {
+    return this.url.replace('/upload', '/upload/w_200');
+})
+
 const CampgroundSchema = new Schema({
     title: String,
-    images: [
-        {
-            url: String,
-            filename: String
-        }
-    ],
+    images: [ImageSchema],
     price: {
         type: Number,
         //min: 0
