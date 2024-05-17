ff3a79040c8f600120a75596e092f9f73927fb10
Ramya Singavarapu <ramyalakshmisingavarapu@gmail.com>
2024-05-17 10:41:14 +0200

    Displaying a map

---
+++ YelpCamp/public/javascripts/showPageMap.js
@@ -0,0 +1,7 @@
+mapboxgl.accessToken = mapToken;
+const map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/streets-v12',
+    center: [-74.5, 40],
+    zoom: 9,
+});
