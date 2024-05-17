5d553662c6fff023c2a649d7ef3643bf21cdcc0a
Ramya Singavarapu <ramyalakshmisingavarapu@gmail.com>
2024-05-17 11:12:31 +0200

    Centering the map on a campground

--- YelpCamp/public/javascripts/showPageMap.js
+++ YelpCamp/public/javascripts/showPageMap.js
@@ -2,6 +2,10 @@ mapboxgl.accessToken = mapToken;
 const map = new mapboxgl.Map({
     container: 'map',
     style: 'mapbox://styles/mapbox/streets-v12',
-    center: [-74.5, 40],
-    zoom: 9,
+    center: campground.geometry.coordinates,
+    zoom: 8,
 });
+
+new mapboxgl.Marker()
+    .setLngLat(campground.geometry.coordinates)
+    .addTo(map)
