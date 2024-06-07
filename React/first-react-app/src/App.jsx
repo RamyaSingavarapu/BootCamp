1fc45650781fdf494979cbfe7a19c668d8eb7a21
Ramya Singavarapu <ramyalakshmisingavarapu@gmail.com>
2024-06-07 17:07:29 +0200

    React Conditionals

--- React/first-react-app/src/App.jsx
+++ React/first-react-app/src/App.jsx
@@ -3,18 +3,22 @@ import Greeter from "./Greeter"
 import Chicken from "./Chicken"
 import Die from "./Die"
 import ListPicker from "./ListPicker"
+import DoubleDice from "./DoubleDice"
 
 function App() {
   return (
     <div>
+      <DoubleDice />
+      <DoubleDice />
+      <DoubleDice />
       {/* <Greeter from="Dinesh" />
       <Greeter person="Chubby" />
       <Greeter person="Laddu" from="Mammy" />
       <Die side={20} />
       <Die />
       <Die side={10} /> */}
-      <ListPicker values={[1, 2, 3]} />
-      <ListPicker values={["a", "b", "c"]} />
+      {/* <ListPicker values={[1, 2, 3]} />
+      <ListPicker values={["a", "b", "c"]} /> */}
 
     </div>
   );
---
+++ React/first-react-app/src/DoubleDice.jsx
@@ -0,0 +1,64 @@
+
+export default function DoubleDice() {
+    const num1 = Math.floor((Math.random() * 3) + 1);
+    const num2 = Math.floor((Math.random() * 3) + 1);
+    return (
+        <div>
+            <h2>Double Dice</h2>
+            {num1 === num2 && <h3>"YOU WIN!!"</h3>}
+            <p>Num1 : {num1}</p>
+            <p>Num2 : {num2}</p>
+        </div>
+    )
+}
+
+
+
+
+// export default function DoubleDice() {
+//     const num1 = Math.floor((Math.random() * 3) + 1);
+//     const num2 = Math.floor((Math.random() * 3) + 1);
+//     return (
+//         <div>
+//             <h2>Double Dice</h2>
+//             {num1 === num2 ? <h3>"YOU WIN!!"</h3> : null}
+//             <p>Num1 : {num1}</p>
+//             <p>Num2 : {num2}</p>
+//         </div>
+//     )
+// }
+
+
+
+
+
+// export default function DoubleDice() {
+//     const num1 = Math.floor((Math.random() * 3) + 1);
+//     const num2 = Math.floor((Math.random() * 3) + 1);
+
+//     return (
+//         <div>
+//             <h2>{num1 === num2 ? "YOU WIN!!" : "YOU LOOSE :("}</h2>
+//             <p>Num1 : {num1}</p>
+//             <p>Num2 : {num2}</p>
+//         </div>
+//     )
+// }
+
+
+
+
+
+
+// export default function DoubleDice() {
+//     const num1 = Math.floor((Math.random() * 3) + 1);
+//     const num2 = Math.floor((Math.random() * 3) + 1);
+//     const result = num1 === num2 ? "YOU WIN!!" : "YOU LOOSE :(";
+//     return (
+//         <div>
+//             <h2>{result}</h2>
+//             <p>Num1 : {num1}</p>
+//             <p>Num2 : {num2}</p>
+//         </div>
+//     )
+// }
