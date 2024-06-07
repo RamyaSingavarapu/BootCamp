26b1a83c3047436e47e0a6a9bd3450dd757be2ad
Ramya Singavarapu <ramyalakshmisingavarapu@gmail.com>
2024-06-07 16:06:47 +0200

    Introducing Props into code

---
+++ React/first-react-app/src/Chicken.jsx
@@ -0,0 +1,5 @@
+import "./Chicken.css"
+
+export default function Chicken() {
+    return <p className="Chicken">Bock!!Bock!!Bock!!</p>
+}
---
+++ React/first-react-app/src/Greeter.jsx
@@ -0,0 +1,5 @@
+export default function Greeter({ person }) {
+
+    return <h1>Hey there!{person} </h1>
+}
+
