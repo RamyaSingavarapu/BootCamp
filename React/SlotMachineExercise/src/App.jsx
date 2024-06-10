5e2313eb0b71712185a05af593b6f5bfbb905c23
Ramya Singavarapu <ramyalakshmisingavarapu@gmail.com>
2024-06-10 11:16:26 +0200

    Implemented basic slot machine logic

---
+++ React/SlotMachineExercise/src/App.jsx
@@ -0,0 +1,14 @@
+import Slots from "./Slots"
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <Slots val1="🍌" val2="🍌" val3="🍌" />
+      <Slots val1="🍒" val2="🍌" val3="🍒" />
+    </div>
+  )
+
+}
+
+export default App
