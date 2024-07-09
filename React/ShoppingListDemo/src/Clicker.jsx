7e2c9afe99e59eca026c75776997d174d5e4d1f7
Ramya Singavarapu <ramyalakshmisingavarapu@gmail.com>
2024-07-09 12:52:01 +0200

    Non-click events

--- React/ShoppingListDemo/src/Clicker.jsx
+++ React/ShoppingListDemo/src/Clicker.jsx
@@ -1,11 +1,14 @@
 function handleClick() {
     console.log("Clicked the button");
 }
+function handleHover() {
+    console.log("HOVERED");
+}
 
 export default function Clicker() {
     return (
         <div>
-            <h2>Click the button</h2>
+            <h2 onMouseOver={handleHover}>Hover over Me</h2>
             <button onClick={handleClick}>Click</button>
         </div>
 
