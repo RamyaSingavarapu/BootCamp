cb16fa0cf2d46fc13e2fb3794c3868301ba58773
Ramya Singavarapu <ramyalakshmisingavarapu@gmail.com>
2024-08-08 22:54:50 +0200

    The Lucky7Game: LuckyN Component

--- React/Lucky7Game/src/App.jsx
+++ React/Lucky7Game/src/App.jsx
@@ -1,11 +1,10 @@
-
 import './App.css'
 import LuckyN from "./LuckyN"
-
 function App() {
   return (
     <>
       <LuckyN />
+      <LuckyN numDice={4} goal={11} />
 
     </>
   )
--- React/Lucky7Game/src/LuckyN.jsx
+++ React/Lucky7Game/src/LuckyN.jsx
@@ -1,12 +1,15 @@
 import { useState } from "react"
-import { getRolls } from "./utilis"
+import { getRolls, sum } from "./utilis"
 import Dice from "./Dice"
 function LuckyN({ numDice = 2, goal = 7 }) {
     const [dice, setDice] = useState(getRolls(numDice));
+    const isWinner = sum(dice) === goal;
+    const roll = () => setDice(getRolls(numDice));
     return (
         <main className="LuckyN">
-            <h1>Lucky{goal}</h1>
+            <h1>Lucky{goal}{isWinner && "You Win!"}</h1>
             <Dice dice={dice} />
+            <button onClick={roll}>Re-Roll Dice</button>
         </main>
     )
 }
