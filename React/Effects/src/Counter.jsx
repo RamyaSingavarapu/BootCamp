76069fb9f669c9d2d6f9ccd3ec7efb8b4ac9ad09
Ramya Singavarapu <ramyalakshmisingavarapu@gmail.com>
2024-08-11 01:56:38 +0200

    useEffect Dependencies

---
+++ React/Effects/src/Counter.jsx
@@ -0,0 +1,25 @@
+import { useState, useEffect } from "react"
+
+function Counter() {
+    const [count, setCount] = useState(0);
+    const [name, setName] = useState("");
+    useEffect(function myEffect() {
+        console.log("MY EFFECT IS CALLED")
+    }, [])
+    const incrementCount = () => {
+        setCount((c) => c + 1)
+    }
+    const handleChange = (e) => {
+        setName(e.target.value)
+    }
+    return (
+        <>
+            <h1>{count}</h1>
+            <button onClick={incrementCount}>+1</button>
+            <h1>Name:{name}</h1>
+            <input type="text" value={name} onChange={handleChange} />
+        </>
+    )
+}
+
+export default Counter;
