143f1c895c2b84b8fc2b73670b7959426b327655
Ramya Singavarapu <ramyalakshmisingavarapu@gmail.com>
2024-09-09 15:27:13 +0200

    Adding a Loader

--- React/Effects/src/App.jsx
+++ React/Effects/src/App.jsx
@@ -1,13 +1,15 @@
 import './App.css'
 import Counter from "./Counter"
 import QuoteFetcher from "./QuoteFetcher"
+import QuoteFetcherLoader from "./QuoteFetcherLoader"
 
 function App() {
 
   return (
     <>
       {/* <Counter /> */}
-      <QuoteFetcher />
+      {/* <QuoteFetcher /> */}
+      <QuoteFetcherLoader />
     </>
   )
 }
---
+++ React/Effects/src/QuoteFetcherLoader.jsx
@@ -0,0 +1,26 @@
+import { useState, useEffect } from "react"
+import './App.css'
+const RANDOM_QUOTE_URL = "https://inspo-quotes-api.herokuapp.com/quotes/random";
+
+export default function QuoteFetcherLoader() {
+    const [quote, setQuote] = useState({ text: "", author: "" })
+    const [isLoading, setIsLoading] = useState(true)
+    useEffect(() => {
+        async function fetchQuote() {
+            const response = await fetch(RANDOM_QUOTE_URL)
+            const jsonResponse = await response.json();
+            const randomQuote = jsonResponse.quote
+            setQuote(randomQuote)
+            setIsLoading(false)
+        }
+        fetchQuote();
+    }, [])
+
+    return (
+        <div>
+            <p className="Loader" style={{ opacity: isLoading ? 1 : 0 }}>Loading...</p>
+            <h1>{quote.text}</h1>
+            <h3>{quote.author}</h3>
+        </div>
+    )
+}
