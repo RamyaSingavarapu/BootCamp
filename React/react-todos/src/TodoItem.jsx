c8503f900c1d673d703c9e8012a95d0eaf9b1be1
Ramya Singavarapu <ramyalakshmisingavarapu@gmail.com>
2024-09-10 13:37:06 +0200

    Removing Todos

---
+++ React/react-todos/src/TodoItem.jsx
@@ -0,0 +1,34 @@
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import Checkbox from '@mui/material/Checkbox';
+import IconButton from '@mui/material/IconButton';
+import CommentIcon from '@mui/icons-material/Comment';
+export default function TodoItem({ todo, removeTodo }) {
+    const labelId = `checkbox-list-label-${todo.id}`;
+    return (
+
+        <ListItem
+            secondaryAction={
+                <IconButton edge="end" aria-label="comments" onClick={removeTodo}>
+                    <CommentIcon />
+                </IconButton>
+            }
+            disablePadding
+        >
+            <ListItemButton role={undefined} dense>
+                <ListItemIcon>
+                    <Checkbox
+                        edge="start"
+                        checked={todo.completed}
+                        tabIndex={-1}
+                        disableRipple
+                        inputProps={{ 'aria-labelledby': labelId }}
+                    />
+                </ListItemIcon>
+                <ListItemText id={labelId} primary={todo.text} />
+            </ListItemButton>
+        </ListItem>
+    )
+}
--- React/react-todos/src/TodoList.jsx
+++ React/react-todos/src/TodoList.jsx
@@ -1,12 +1,7 @@
 import { useState } from "react"
 import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
-import Checkbox from '@mui/material/Checkbox';
-import IconButton from '@mui/material/IconButton';
-import CommentIcon from '@mui/icons-material/Comment';
+import TodoItem from "./TodoItem";
+
 const initialTodos = [
     { id: 1, text: "walk the dog", completed: false },
     { id: 3, text: "walk the cat", completed: false },
@@ -16,36 +11,19 @@ const initialTodos = [
 
 export default function TodoList() {
     const [todos, setTodos] = useState(initialTodos)
+    const removeTodo = (id) => {
+        setTodos((prevTodos) => {
+            return prevTodos.filter((t) => t.id !== id)
+        })
+    }
     return (
         <>
             <List sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
-                {todos.map((todo) => {
-                    const labelId = `checkbox-list-label-${todo.id}`;
-                    return (
-                        <ListItem
-                            key={todo.id}
-                            secondaryAction={
-                                <IconButton edge="end" aria-label="comments">
-                                    <CommentIcon />
-                                </IconButton>
-                            }
-                            disablePadding
-                        >
-                            <ListItemButton role={undefined} dense>
-                                <ListItemIcon>
-                                    <Checkbox
-                                        edge="start"
-                                        checked={todo.completed}
-                                        tabIndex={-1}
-                                        disableRipple
-                                        inputProps={{ 'aria-labelledby': labelId }}
-                                    />
-                                </ListItemIcon>
-                                <ListItemText id={labelId} primary={todo.text} />
-                            </ListItemButton>
-                        </ListItem>
-                    )
-                })}
+                {todos.map((todo) => (
+                    <TodoItem todo={todo} key={todo.id} removeTodo={() => removeTodo(todo.id)} />
+                )
+
+                )}
             </List>
         </>
     )
