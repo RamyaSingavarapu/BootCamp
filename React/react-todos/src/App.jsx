affb8583f8b59a7031c22f77cdfa87c1c62e9010
Ramya Singavarapu <ramyalakshmisingavarapu@gmail.com>
2024-09-10 11:33:24 +0200

    Creating Todo application

---
+++ React/react-todos/src/App.jsx
@@ -0,0 +1,15 @@
+import CssBaseline from "@mui/material/CssBaseline"
+
+function App() {
+
+
+  return (
+    <>
+      <CssBaseline />
+      <h1>Todos</h1>
+
+    </>
+  )
+}
+
+export default App
