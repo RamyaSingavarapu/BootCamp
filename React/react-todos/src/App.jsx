90c39ddafd3a2346b4db48370a3874daf147b11b
Ramya Singavarapu <ramyalakshmisingavarapu@gmail.com>
2024-09-10 12:03:00 +0200

    The TodoList component

--- React/react-todos/src/App.jsx
+++ React/react-todos/src/App.jsx
@@ -1,5 +1,5 @@
 import CssBaseline from "@mui/material/CssBaseline"
-
+import TodoList from "./TodoList"
 function App() {
 
 
@@ -7,6 +7,7 @@ function App() {
     <>
       <CssBaseline />
       <h1>Todos</h1>
+      <TodoList />
 
     </>
   )
